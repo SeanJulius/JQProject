7f6513795b528af2b1755f3286388543eb20dae2
Sean <crimsonsand@live.com>
2022-03-30 20:45:49 -0700

    Add css styles

--- js/scripts.js
+++ js/scripts.js
@@ -22,3 +22,20 @@ $(document).ready(function() {
     $("#walrus-hidden").slideToggle();
   });
 });
+
+$(document).ready(function() {
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
+});
